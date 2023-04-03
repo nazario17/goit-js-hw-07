ac46701a8efc2f4178921f91eccb700f22530f7f
nazario17 <hazarkorchak@gmail.com>
2023-04-03 18:23:54 +0300

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,14 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const container = document.querySelector(".gallery");
-const markup = galleryItems.map(({ preview, description }) => { 
-    `<li class = "gallery__item">
-      <img src="${preview}" alt="${description}">
-    </li>`;
-});
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item"> 
+            <a class="gallery__link" href="${original}" >  
+            <img class="gallery__image" src="${preview}" alt="${description}" data-source="${original}" />  
+            </a>    
+           </li>`
+  )
+  .join("");
 
+container.insertAdjacentHTML("beforeend", markup);
+container.addEventListener("click", onGalleryClick);
 
-container.insertAdjacentHTML("beforeend", markup.join(""));
+function onGalleryClick(event) {
+  event.preventDefault();
 
-// console.log(galleryItems);
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const modal = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" alt="${event.target.alt}">`
+  );
+    modal.show();
+
+    document.addEventListener("keydown", (event) => {
+      if (event.key === "Escape") {
+        modal.close();
+      }
+    });
+}
