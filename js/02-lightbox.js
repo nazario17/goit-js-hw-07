b30df4e34f4a0a744d99d40e2d328f68b4e1d8bb
nazario17 <hazarkorchak@gmail.com>
2023-04-04 19:14:56 +0300

    task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,49 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const container = document.querySelector(".gallery");
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item"> 
+            <a class="gallery__link" href="${original}" >  
+            <img class="gallery__image" src="${preview}" alt="${description}" data-source="${original}" />  
+            </a>    
+           </li>`
+  )
+  .join("");
+
+container.insertAdjacentHTML("beforeend", markup);
+container.addEventListener("click", onGalleryClick);
+
+function onGalleryClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const modal = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" alt="${event.target.alt}">`
+  );
+  modal.show();
+
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+      modal.close();
+    }
+  });
+}
+
+
+
+function initLightbox() {
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captions: true,
+    captionDelay: 250,
+    captionsData: "alt",
+    captionPosition: "bottom",
+  });
+}
+
+initLightbox();
